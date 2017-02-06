492a11f3c9faf3d3d5a2a9a28a40c56f883c329d
Mario de Biase <debiasej@gmail.com>
2017-02-06 20:54:30 +0100

    Hook jquery up with the created fake DOM

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,18 @@
 import jsom from 'jsdom';
+import jquery from 'jquery';
 
+/* Set up testing environment to run like a browser in the command line,
+ for when we run Mocka:*/
 
-// Set up testing environment to run like a browser in the command line:
 // global is the global variable because of my tests are running under node.js
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html)>');
+global.window = global.document.defaultView;
+
+// Set the (fake) window object that jquery has to be responsible for. 
+const $ = jquery(global.window);
 
 
-
-
-// Build 'renderComponent' helper that should render a given react ComponentClass
+/* Build 'renderComponent' helper that should render a given react ComponentClass */
 
 
 
