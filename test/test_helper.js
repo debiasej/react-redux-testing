693d11a059ce7c6063b98aab26f84758df2e0d7d
Mario de Biase <debiasej@gmail.com>
2017-02-06 20:27:31 +0100

    Use jsdom to emulate the behaviour of the DOM in the Terminal

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,9 @@
+import jsom from 'jsdom';
 
 
-// Set up testing environment to run like a browser in the command line
-
+// Set up testing environment to run like a browser in the command line:
+// global is the global variable because of my tests are running under node.js
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html)>');
 
 
 
